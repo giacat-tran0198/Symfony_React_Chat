dc208dcce1d150bd2dfd5a92e12300201f8bfda4
ilove3q <ilove3q@gmail.com>
2021-06-06 17:48:08 +0200

    update entity and auth

---
+++ src/Entity/Participant.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ParticipantRepository::class)
+ */
+class Participant
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="participants")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Conversation", inversedBy="participants")
+     */
+    private $conversation;
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getConversation(): ?Conversation
+    {
+        return $this->conversation;
+    }
+
+    public function setConversation(?Conversation $conversation): self
+    {
+        $this->conversation = $conversation;
+
+        return $this;
+    }
+}
